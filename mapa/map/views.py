2d1f7b63f21e53bbff69d4c61348d364d281f0d2
Enzo Coelho Morona <moronaenzo@gmail.com>
2024-03-05 15:28:10 -0300

    day 2

--- mapa/map/views.py
+++ mapa/map/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.core.serializers import serialize
+from .models import Post
+import json
 # Create your views here.
 def map(request):
     return render(request, 'map/map.html')
 
+
 def save_point(request):
     if request.method == 'POST':
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-        # Faça o que quiser com as coordenadas, como salvar no banco de dados
-        # Por exemplo:
-        # Point.objects.create(latitude=latitude, longitude=longitude)
-        return JsonResponse({'success': True})
+        # Verificar se há dados no corpo da solicitação
+        if request.body:
+            # Decodificar os dados JSON do corpo da solicitação
+            data = json.loads(request.body)
+            # Aqui você pode acessar os dados conforme necessário
+            print(data)
+            # Você pode manipular os dados aqui e, em seguida, retornar uma resposta JSON
+            # Exemplo de manipulação de dados:
+            response_data = {'status': 'success', 'message': 'Dados recebidos com sucesso'}
+            return JsonResponse(response_data)
     else:
         return JsonResponse({'success': False, 'error': 'Método não permitido'})

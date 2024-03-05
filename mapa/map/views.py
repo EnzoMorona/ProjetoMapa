7a1451df6480244946cab04274823a1fdc312149
Enzo Coelho Morona <moronaenzo@gmail.com>
2024-03-05 10:36:16 -0300

    day 2

--- mapa/map/views.py
+++ mapa/map/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import Post
 # Create your views here.
 def map(request):
     return render(request, 'map/map.html')
 
 def save_point(request):
     if request.method == 'POST':
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
+        json = Post.objects.all()
         # Faça o que quiser com as coordenadas, como salvar no banco de dados
         # Por exemplo:
         # Point.objects.create(latitude=latitude, longitude=longitude)
-        return JsonResponse({'success': True})
+        json.save()
+        return render(request, 'map.html', {'json': json})
     else:
         return JsonResponse({'success': False, 'error': 'Método não permitido'})
